bcd91e4e4eca95bd30c4e5f3579d1064c1d10d93
Stevo99 <Sfailla1983@gmail.com>
2019-08-12 21:55:35 -0400

    changes to file structure. removed unnecessary files

---
+++ src/App/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import Welcome from './WelcomeMessage';
+
+class App extends Component {
+	render() {
+		return <Welcome />;
+	}
+}
+
+export default App;
